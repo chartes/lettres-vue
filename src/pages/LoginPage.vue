0783de85eb81030811a68132f39c3df4f9062778
Julien Pilla <vrlitwhai.kridanik@gmail.com>
2020-06-30 13:28:30 +0200

    style login page

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="container">
     <div
       v-if="current_user"
       class="welcome"
@@ -8,34 +8,47 @@
     </div>
     <form
       v-else
+      class="login-form"
       @submit.prevent="login"
     >
-      <label for="email">
-        Email:
-      </label>
-      <input
-        v-model="email"
-        type="email"
-        name="email"
-        value
-      >
-          
-      <label for="password">
-        Password:
-      </label>
-      <input
-        v-model="password"
-        type="password"
-        name="password"
-        value
-      >
-      
-      <button
-        type="submit"
-        name="login-btn"
-      >
-        Login
-      </button>
+      <div class="field">
+        <p class="control has-icons-left has-icons-right">
+          <input
+            v-model="email"
+            class="input"
+            type="email"
+            placeholder="Email"
+          >
+          <span class="icon is-small is-left">
+            <i class="fas fa-envelope" />
+          </span>
+          <span class="icon is-small is-right">
+            <i class="fas fa-check" />
+          </span>
+        </p>
+      </div>
+      <div class="field">
+        <p class="control has-icons-left">
+          <input
+            v-model="password"
+            class="input"
+            type="password"
+            placeholder="Mot de passe"
+            value
+            name="password"
+          >
+          <span class="icon is-small is-left">
+            <i class="fas fa-lock" />
+          </span>
+        </p>
+      </div>
+      <div class="field">
+        <p class="control">
+          <button class="button is-primary">
+            Connexion
+          </button>
+        </p>
+      </div>
     </form>
   </div>
 </template>
@@ -86,6 +99,10 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped lang="scss">
+@import "@/assets/sass/main.scss";
+.login-form {
+  margin-top: 30px;
+  width: 400px;
+}
 </style>
