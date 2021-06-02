6edf086821d8129e85004c48807f0378c4684c92
Julien Pilla <vrlitwhai.kridanik@gmail.com>
2021-06-02 18:01:24 +0200

    wip b-table

--- src/components/sections/DocumentList.vue
+++ src/components/sections/DocumentList.vue
@@ -1,99 +1,94 @@
 <template>
   <div class="section">
     <div class="container">
-      <div v-show="!loadingStatus">
-        <div
-          v-show="documents"
-          class="has-text-centered"
+      <b-table
+        :data="tableData"
+        :loading="loadingStatus"
+
+        paginated
+        backend-pagination
+        :total="totalCount"
+        :per-page="pageSize"
+        :current-page.sync="currentPage"
+        pagination-position="both"
+        aria-next-label="Page suivante"
+
+        aria-previous-label="Page précédente"
+        aria-page-label="Page"
+        aria-current-label="Page courante"
+        detailed
+
+        detail-key="id"
+        show-detail-icon
+        :narrowed="true"
+        :hoverable="true"
+
+        :mobile-cards="true"
+        @page-change="onPageChange"
+      >
+        <b-table-column
+          v-slot="props"
+          field="id"
+          label="Document"
+          width="40"
         >
-          <pagination />
-        </div>
-    
-        <b-table
-          :data="tableData"
-          detailed
-          detail-key="id"
-          show-detail-icon
-          :narrowed="true"
-          :hoverable="true"
-          :mobile-cards="true"
-          class="pb-5 pt-5"
-        >
-          <b-table-column
-            v-slot="props"
-            field="id"
-            label="Document"
-            width="40"
+          <span
+            class="tags has-addons document-tag-bar"
           >
-            <span
-              class="tags has-addons document-tag-bar"
+            <router-link
+              :to="{name: 'document', params: {docId: props.row.id}}"
+              class="tag document-preview-card__doc-tag"
             >
-              <router-link
-                :to="{name: 'document', params: {docId: props.row.id}}"
-                class="tag document-preview-card__doc-tag"
-              >
-                <span>Document {{ props.row.id }}</span>
-              </router-link>
-            </span>
-          </b-table-column>
-
-          <b-table-column
-            v-slot="props"
-            field="date"
-            label="Date de rédaction"
-            :td-attrs="columnTdAttrs"
-          >
-            {{ props.row.date }}
-          </b-table-column>
-
-          <b-table-column
-            v-slot="props"
-            field="title"
-            label="Titre"
-          >
-            <router-link :to="{ name: 'document', params: { docId: props.row.id } }">
-              <h2
-                class="document-preview-card__title"
-                v-html="props.row.title"
-              />
+              <span>Document {{ props.row.id }}</span>
             </router-link>
-          </b-table-column>
+          </span>
+        </b-table-column>
 
-          <template #empty>
-            <div class="has-text-centered">
-              Aucun résultat
-            </div>
-          </template>
-
-          <template #detail="props">
-            <document-preview-card
-              :doc-id="props.row.id"
-            />
-          </template>
-        </b-table>
-        
-
-        <div
-          v-show="documents"
-          class="has-text-centered"
+        <b-table-column
+          v-slot="props"
+          field="date"
+          label="Date de rédaction"
+          :td-attrs="columnTdAttrs"
         >
-          <pagination />
-        </div>
-      </div>
+          {{ props.row.date }}
+        </b-table-column>
+
+        <b-table-column
+          v-slot="props"
+          field="title"
+          label="Titre"
+        >
+          <router-link :to="{ name: 'document', params: { docId: props.row.id } }">
+            <h2
+              class="document-preview-card__title"
+              v-html="props.row.title"
+            />
+          </router-link>
+        </b-table-column>
+
+        <template #empty>
+          <div class="has-text-centered">
+            Aucun résultat
+          </div>
+        </template>
+
+        <template #detail="props">
+          <document-preview-card
+            :doc-id="props.row.id"
+          />
+        </template>
+      </b-table>
     </div>
   </div>
 </template>
 
 <script>
 import { mapState, mapActions } from "vuex";
-import {http} from "@/modules/http-common";
-import Pagination from "../ui/Pagination";
 
 export default {
   name: "DocumentList",
   components: {
-    DocumentPreviewCard: () => import("../document/DocumentPreviewCard"),
-    Pagination,
+    DocumentPreviewCard: () => import("../document/DocumentPreviewCard")
   },
   props: {},
   data() {
@@ -102,25 +97,37 @@ export default {
     };
   },
   computed: {
-    ...mapState("search", ["documents", "loadingStatus"]),
+    ...mapState("search", ["documents", "loadingStatus", "numPage", "pageSize", "totalCount"]),
     ...mapState("user", ["current_user"]),
 
+    currentPage: {
+        get: function () {
+          return this.numPage
+        },
+        set: function (newValue) {
+          console.log("setting new page")
+          this.setNumPage(newValue)
+          this.performSearch()
+          this.loadAsyncData()
+        }
+    }
   },
   watch: {
     documents() {
-      this.asyncLoadTable()
+      this.loadAsyncData()
     }
   },
   created() {
-    this.asyncLoadTable()
+    this.loadAsyncData()
   },
   methods: {
-    async asyncLoadTable() {
+    ...mapActions("search", ["setNumPage", "performSearch"]),
+
+    async loadAsyncData() {
       this.tableData = await Promise.all(this.documents.map(async d => {
         //const witnesses = await http.get(`documents/${d.id}/witnesses?without-relationships`)
         return {
           id: d.id,
-          tag: 'tag',
           title :d.attributes.title,
           date : d.attributes.creation,
           //witnesses: witnesses.data.data
@@ -130,9 +137,9 @@ export default {
     columnTdAttrs(row, column) {
       if (column.label === 'Date de rédaction') {
         return {
-          class: 'has-text-weight-bold',
+          class: '',
           style: {
-            'min-width': '120px'
+            'min-width': '180px'
           }
         }
       }
@@ -140,6 +147,12 @@ export default {
         return null
       }
     },
+         /*
+        * Handle page-change event
+        */
+            onPageChange(page) {
+                this.currentPage = page
+            },
   }
 };
 </script>
