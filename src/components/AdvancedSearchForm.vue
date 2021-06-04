31f5546cdda52cb9c28e2e9f6703da4f747afd9c
Julien Pilla <vrlitwhai.kridanik@gmail.com>
2021-06-04 11:07:06 +0200

    add wip slider

--- src/components/AdvancedSearchForm.vue
+++ src/components/AdvancedSearchForm.vue
@@ -14,7 +14,84 @@
         class="mt-5 mb-2"
       />
 
-      Formulaire de recherche avancée
+      <p>Formulaire de recherche avancée (en cours de développement)</p>
+
+      <div class="field date-slider">
+        <span class="label">Date de rédaction</span>
+        <b-field>
+          <b-slider
+            v-model="creationDateSelection"
+            tooltip-always
+            size="is-medium"
+            :min="1300"
+            :max="1600"
+          >
+            <template v-for="val in [1300, 1400, 1500, 1600]">
+              <b-slider-tick
+                :key="val"
+                :value="val"
+              >
+                {{ val }}
+              </b-slider-tick>
+            </template>
+          </b-slider>
+        </b-field>
+      </div>
+
+      <div class="field">
+        <div class="field has-addons">
+          <div class="control is-expanded">
+            <div class="select is-fullwidth">
+              <select name="country">
+                <option value="Argentina">
+                  Argentina
+                </option>
+                <option value="Bolivia">
+                  Bolivia
+                </option>
+                <option value="Brazil">
+                  Brazil
+                </option>
+                <option value="Chile">
+                  Chile
+                </option>
+                <option value="Colombia">
+                  Colombia
+                </option>
+                <option value="Ecuador">
+                  Ecuador
+                </option>
+                <option value="Guyana">
+                  Guyana
+                </option>
+                <option value="Paraguay">
+                  Paraguay
+                </option>
+                <option value="Peru">
+                  Peru
+                </option>
+                <option value="Suriname">
+                  Suriname
+                </option>
+                <option value="Uruguay">
+                  Uruguay
+                </option>
+                <option value="Venezuela">
+                  Venezuela
+                </option>
+              </select>
+            </div>
+          </div>
+          <div class="control">
+            <button
+              type="submit"
+              class="button is-primary"
+            >
+              Choose
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 </template>
@@ -28,6 +105,11 @@ export default {
     components: {
       SearchBox
     },
+    data() {
+        return {
+          creationDateSelection: [1370, 1415],
+        }
+      },
     computed: {
       ...mapState('layout', ['showLeftSideBar'])
     },
@@ -44,6 +126,10 @@ export default {
 <style lang="scss" scoped>
 @import "@/assets/sass/main.scss";
 
+.b-slider .b-slider-thumb-wrapper.has-indicator .b-slider-thumb {
+  padding: 14px 4px !important;
+}
+
 .close-form {
     font-size: 28px;
     
@@ -55,4 +141,17 @@ export default {
       cursor: pointer;
     }
 }
+
+.date-slider {
+    margin-top: 40px;
+    margin-bottom: 40px !important;
+    .label {
+      margin-bottom: 50px;
+    }
+   .field {
+      margin-left: 20px;
+      margin-right: 10px;
+   }
+}
+
 </style>
