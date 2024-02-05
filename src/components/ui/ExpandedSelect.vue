ef7006907b31112e4207dfa036a2c8d658969abd
vicpsl <jvboby@gmail.com>
2024-02-05 16:43:35 +0100

    correct place and person wizards, enhance design and responsive css

--- src/components/ui/ExpandedSelect.vue
+++ src/components/ui/ExpandedSelect.vue
@@ -1,5 +1,8 @@
 <template>
-  <dl class="expanded-select">
+  <dl
+    v-if="items.length > 0"
+    class="expanded-select"
+  >
     <dt
       v-for="(item, i) in items"
       :key="item"
@@ -7,6 +10,14 @@
       @click="toggleSelection(i)"
     >
       {{ item }}
+    </dt>
+  </dl>
+  <dl
+    v-else
+    class="expanded-select"
+  >
+    <dt>
+      Aucune suggestion
     </dt>
   </dl>
 </template>
@@ -17,6 +28,7 @@ export default {
   props: {
     items: { type: Array, default: () => [] },
     selectedIndex: { type: Number, default: null },
+    updatedFunction: { type: String, default: ''}
   },
   emits: ["changed"],
   data() {
@@ -25,6 +37,12 @@ export default {
     };
   },
   watch: {
+    items(newVal, OldVal) {
+      if (this.$props.items.length === 1 && this.$props.items[0].toLowerCase().normalize("NFD").replace(/\p{Diacritic}/gu, "") === this.$props.updatedFunction.toLowerCase().normalize("NFD").replace(/\p{Diacritic}/gu, "")) {
+        this.setSelection(this.selectedIndex);
+      } else this.selection = null
+
+    },
     selectedIndex() {
       if (this.selectedIndex !== this.selection) {
         this.setSelection(this.selectedIndex);
@@ -40,6 +58,7 @@ export default {
     },
     toggleSelection(i) {
       this.selection = this.selection === i ? null : i;
+      console.log("toggleSelection", this.selection, i, this.$props.items)
       this.$emit(
         "changed",
         this.selection === null
@@ -62,7 +81,7 @@ export default {
 
 .expanded-select {
   min-width: 120px;
-  min-height: 120px;
+  min-height: 0px;
 
   display: flex;
   flex-direction: column;
