36ac804f632c16c3aad5e9e9413f0f115df32d8a
Arnaud Vergnet <arnaud.vergnet@mailo.com>
2024-03-18 14:46:36 +0100

    Create component for create button

---
+++ src/components/ui/CreateButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <b-button
+    type="is-primary"
+    :label="label"
+    class="create-btn"
+    @click="click"
+  />
+</template>
+
+<script>
+export default {
+  name: "CreateButton",
+  props: {
+    label: {type: String, required: true},
+  },
+  methods: {
+    click(evt) {
+      this.$emit('click', evt);
+    }
+  }
+}
+</script>
+
+
+<style lang="scss">
+@import "@/assets/sass/main.scss";
+@import "@/assets/sass/components/_buttons.scss";
+</style>
