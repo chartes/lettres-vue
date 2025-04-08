7ba93bd855dc3c35455eeaa8c02b9e25ac9848c1
vicpsl <jvboby@gmail.com>
2025-04-08 13:37:18 +0200

    updates to About Members

--- src/pages/about/MembersSection.vue
+++ src/pages/about/MembersSection.vue
@@ -5,13 +5,20 @@
     </h1>-->
     <h2 class="subtitle">Les membres</h2>
     <section>
+      <p class="has-text-justified mb-2">
+        L'application <a href="https://ecco.chartes.psl.eu/">ecco</a> est une réalisation de la mission projet numériques de l'École nationale des chartes - PSL.
+      </p>
       <h5 class="subtitle is-5 mt-5 mb-5">Direction scientifique</h5>
       <p class="has-text-justified mb-2">
-        Olivier Poncet (2018-)
+        Olivier Poncet
       </p>
-      <h5 class="subtitle is-5 mt-5 mb-5">Conception, développement et maintenance de l’application</h5>
+      <h5 class="subtitle is-5 mt-5 mb-5">Conception et développement de l’application</h5>
       <p class="has-text-justified mb-2">
-        Vincent Jolivet (2018-), Julien Pilla (2020-2022), Jean-Victor Boby (2022-).
+        Vincent Jolivet, Julien Pilla, Jean-Victor Boby.
+      </p>
+      <h5 class="subtitle is-5 mt-5 mb-5">Hébergement</h5>
+      <p class="has-text-justified mb-2">
+        <a target="_blank" href="https://www.huma-num.fr/">IR* Huma-Num</a>.
       </p>
     </section>
   </article>
